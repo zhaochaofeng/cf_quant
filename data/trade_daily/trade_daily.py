b5e60608f4499ee832d275e4ee2cfc736c81a1b2
zhaochaofeng <469293319@qq.com>
2025-08-20 16:32:29 +0800

    日级交易数据（非复权）

--- data/trade_daily/trade_daily.py
+++ data/trade_daily/trade_daily.py
@@ -1,6 +1,7 @@
 '''
     功能：tushare表导入mysql
-    描述：日级交易数据，取数周期为365天，每日覆盖更新
+    描述：日级交易数据（非复权）+ 复权因子
+    刷数：长周期则设置[start_date, end_date]，短周期则按天刷
 '''
 
 import time
@@ -29,7 +30,8 @@ fea_to_from = {
      'change': 'change',
      'pct_chg': 'pct_chg',
      'vol': 'vol',
-     'amount': 'amount'
+     'amount': 'amount',
+     'adj_factor': 'adj_factor'
 }
 
 def parse_line(row, fea_to_from):
@@ -41,48 +43,88 @@ def parse_line(row, fea_to_from):
             if f == 'day':
                 # 日期格式转换
                 v = datetime.strptime(v, '%Y%m%d').strftime('%Y-%m-%d')
+            if f == 'pct_chg':
+                # 处理pct_chg超出范围的情况
+                if pd.isna(v):
+                    v = None
+                elif abs(v) > 9999.99:
+                    print(f"警告: pct_chg值 {v} 超出范围，已截断为9999.99或-9999.99")
+                    print(row)
+                    v = 9999.99 if v > 0 else -9999.99
             if pd.isna(v):
                 v = None
             tmp[f] = v
         except Exception as e:
             print('except: {}'.format(e))
+            print(row)
     return tmp
 
 def request_from_tushare(ts_codes):
     # 从tushare API获取数据
     print('-' * 100)
     print('从tushare API获取数据...')
-    start_date = (datetime.strptime(args.date, '%Y-%m-%d') - timedelta(days=365)).strftime('%Y%m%d')
-    end_date = datetime.strptime(args.date, '%Y-%m-%d').strftime('%Y%m%d')
+    start_date = datetime.strptime(args.start_date, '%Y-%m-%d').strftime('%Y%m%d')
+    end_date = datetime.strptime(args.end_date, '%Y-%m-%d').strftime('%Y%m%d')
     print('start_date: {}, end_date: {}'.format(start_date, end_date))
     data = []
-    for i, code in enumerate(ts_codes):
-        # time.sleep(0.1)  # API调用频次：1min不超过700次
-        if (i + 1) % 100 == 0:
-            print('requested num: {}'.format(i + 1))
-        info = ts.pro_bar(ts_code=code, start_date=start_date,
-                          end_date=end_date, asset='E',
-                          adj='qfq', freq='D')
-        if info is None:
-            continue
-        for index, row in info.iterrows():
+
+    if start_date == end_date:
+        # 按天更新数据，则批量请求
+        df = pd.DataFrame()
+        k = len(ts_codes) // 1000 if len(ts_codes) % 1000 == 0 else len(ts_codes) // 1000 + 1
+        # 交易数据
+        for i in range(k):
+            codes = ts_codes[i * 1000: min((i + 1) * 1000, len(ts_codes))]
+            tmp = pro.daily(ts_code=','.join(codes),
+                            start_date=start_date, end_date=end_date)
+            df = pd.concat([df, tmp], axis=0, join='outer')
+        df.set_index(keys=['ts_code', 'trade_date'], inplace=True)
+        print(df.head())
+        # 复权因子
+        factor = pro.adj_factor(trade_date=start_date)
+        factor.set_index(keys=['ts_code', 'trade_date'], inplace=True)
+
+        print(factor.head())
+        # 合并交易数据 和 复权因子
+        merged = pd.concat([df, factor], axis=1, join='inner')
+
+        print(merged.head())
+        print('df shape: {}'.format(df.shape))
+        print('factor shape: {}'.format(factor.shape))
+        print('merged shape: {}'.format(merged.shape))
+        merged.reset_index(inplace=True)
+        for index, row in merged.iterrows():
             try:
                 tmp = parse_line(row, fea_to_from)
                 data.append(tmp)
             except Exception as e:
                 print('except: {}'.format(e))
-                print('code: {}'.format(code))
                 continue
+    else:
+        # 回刷历史数据，按单个ts_code请求
+        for i, code in enumerate(ts_codes):
+            time.sleep(0.1)  # API调用频次：1min不超过1000次
+            if (i + 1) % 100 == 0:
+                print('requested num: {}'.format(i + 1))
+            df = pro.daily(ts_code=code, start_date=start_date, end_date=end_date)
+            factor = pro.adj_factor(ts_code=code, start_date=start_date, end_date=end_date)
+            df.set_index(keys=['ts_code', 'trade_date'], inplace=True)
+            factor.set_index(keys=['ts_code', 'trade_date'], inplace=True)
+            merged = pd.concat([df, factor], axis=1, join='inner')
+            merged.reset_index(inplace=True)
+            for index, row in merged.iterrows():
+                try:
+                    tmp = parse_line(row, fea_to_from)
+                    data.append(tmp)
+                except Exception as e:
+                    print('except: {}'.format(e))
+                    print('code: {}'.format(code))
+                    continue
     return data
 def write_to_mysql(data):
     print('-' * 100)
     print('导入msyql ...')
     conn = mysql_connect()
-    # 先清空表
-    with conn.cursor() as cursor:
-        cursor.execute('delete from trade_daily;')
-        conn.commit()
-        print('清空表成功 !!!')
 
     with conn.cursor() as cursor:
         fea_format = ['%({})s'.format(f) for f in list(fea_to_from.keys())]
@@ -90,16 +132,15 @@ def write_to_mysql(data):
         fea_keys = fea_keys.replace('change', '`change`')
 
         sql = '''
-                    INSERT INTO trade_daily({}) VALUES ({})
+                    INSERT INTO trade_daily2({}) VALUES ({})
                 '''.format(fea_keys, ','.join(fea_format))
 
-        n = len(data) // batch_size if len(data) % batch_size == 0 else len(data) // batch_size + 1
-        print('n: {}'.format(n))
-        for i in range(int(n)):
+        k = len(data) // batch_size if len(data) % batch_size == 0 else len(data) // batch_size + 1
+        for i in range(k):
             try:
                 if i == 0:
                     print(cursor.mogrify(sql, data[0]))
-                cursor.executemany(sql, data[i * batch_size:min((i + 1) * batch_size, len(data))])
+                cursor.executemany(sql, data[i * batch_size: min((i + 1) * batch_size, len(data))])
                 conn.commit()
             except Exception as e:
                 print('except: {}'.format(e))
@@ -130,10 +171,11 @@ def main(args):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--date', type=str, default='2025-07-07')
+    parser.add_argument('--start_date', type=str, default='2025-08-08')
+    parser.add_argument('--end_date', type=str, default='2025-08-08')
     args = parser.parse_args()
     print(args)
-    if not is_trade_day(args.date):
+    if not is_trade_day(args.end_date):
         print('不是交易日，退出！！！')
         exit(0)
     t = time.time()
