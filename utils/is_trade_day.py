449edfbb724659b6263d7e79798682c6dc656719
zhaochaofeng <469293319@qq.com>
2025-08-28 15:36:36 +0800

    judge trade day for shell

--- utils/is_trade_day.py
+++ utils/is_trade_day.py
@@ -8,7 +8,7 @@ from utils import is_trade_day
 if __name__ == '__main__':
     if is_trade_day(sys.argv[1]):
         # 交易日
-        exit(5)
+        exit(0)
     else:
         # 非交易日
-        exit(0)
+        exit(5)
