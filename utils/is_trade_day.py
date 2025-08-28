1058112db2d3e2652ee1724e7771a357cabb6c4c
zhaochaofeng <469293319@qq.com>
2025-08-28 15:32:46 +0800

    judge trade day for shell

---
+++ utils/is_trade_day.py
@@ -0,0 +1,14 @@
+'''
+    功能：用于shell脚本中交易日判断
+'''
+
+import sys
+from utils import is_trade_day
+
+if __name__ == '__main__':
+    if is_trade_day(sys.argv[1]):
+        # 交易日
+        exit(5)
+    else:
+        # 非交易日
+        exit(0)
