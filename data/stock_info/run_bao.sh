d42fb4216448e1b86cbb95bd3b511213087e9ebc
zhaochaofeng <469293319@qq.com>
2025-09-09 20:42:30 +0800

    add two new tables

---
+++ data/stock_info/run_bao.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+source ~/.bashrc
+cur_path=`pwd`
+python_path="/root/anaconda3/envs/python3/bin/python"
+echo 'cur_path: '${cur_path}
+
+dt=`date +%Y-%m-%d`
+echo "dt: "${dt}
+
+${python_path} ${cur_path}/stock_info_bao.py --date ${dt}
+
+# 判断是否执行成功
+if [ $? -eq 0 ]; then
+  echo "执行成功！！！"
+  exit 0
+ else
+  echo "执行失败！！！"
+  exit 1
+fi
