1c43bba78dcb65ce1ccd9aaf29665f79716c51f3
zhaochaofeng <469293319@qq.com>
2025-09-12 14:50:48 +0800

    add field change & volume fq & volume and amount unit

--- data/qlib_online/process.py
+++ data/qlib_online/process.py
@@ -10,10 +10,13 @@ import time
 import fire
 import shutil
 import pandas as pd
+import traceback
+from datetime import datetime
 from utils.utils import tushare_pro
 from utils.utils import sql_engine
 from utils.utils import is_trade_day
-from datetime import datetime
+from utils.utils import send_email
+
 pro = tushare_pro()
 
 class Processor:
@@ -23,18 +26,20 @@ class Processor:
                  start_date: str = None,         # 数据起始日期
                  end_date: str = None,           # 数据终止日期
                  is_offline: bool = False,       # 是否处理批量导出的离线数据
-                 index_code: str = '000300.SH',  # 指数code
+                 index_list: list = None,        # 指数code
                  fq: str = 'hfq',                # 复权方式。hfq: 后复权; qfq: 前复权; None: 不复权
                  columns: list = None
                  ):
         if columns is None:
-            columns = ['ts_code', 'date', 'open', 'close', 'high', 'low', 'volume', 'amount', 'factor']
+            columns = ['ts_code', 'date', 'open', 'close', 'high', 'low', 'volume', 'amount', 'factor', 'change']
+        if index_list is None:
+            index_list = ['000300.SH', '000905.SH', '000903.SH']
         self.provider_uri = provider_uri
         self.path_in = path_in
         self.start_date = start_date
         self.end_date = end_date
         self.is_offline = is_offline
-        self.index_code = index_code
+        self.index_list = index_list
         self.fq = fq
         self.columns = columns
 
@@ -45,6 +50,7 @@ class Processor:
 
     def load_data(self):
         ''' 加载股票数据 '''
+        print('-' * 100)
         print('load_data ...')
         if self.is_offline:
             df = pd.read_csv(self.path_in, sep='\t')
@@ -52,17 +58,19 @@ class Processor:
             engine = sql_engine()
             sql = '''
             select ts_code, day as date, open, close, high, low, vol, amount, adj_factor 
-            from cf_quant.trade_daily2 where day>='{}' and day<='{}';
+            from trade_daily_ts where day>='{}' and day<='{}';
             '''.format(self.start_date, self.end_date)
             print('{}\n{}\n{}'.format('-' * 50, sql, '-' * 50))
             df = pd.read_sql(sql, engine)
             # 将date字段从datetime.date转化为str格式
             df['date'] = pd.to_datetime(df['date'], format='%Y-%m-%d').dt.strftime('%Y-%m-%d')
             df.to_csv(os.path.join(self.provider_uri, 'custom_{}_{}.csv'.format(self.start_date, self.end_date)), sep='\t', index=False)
+
         return df
 
     def load_data_index(self, codes):
         ''' 加载指数数据 '''
+        print('-' * 100)
         print('load_data_index ...')
         start_date = datetime.strptime(self.start_date, '%Y-%m-%d').strftime('%Y%m%d')
         end_date = datetime.strptime(self.end_date, '%Y-%m-%d').strftime('%Y%m%d')
@@ -86,15 +94,12 @@ class Processor:
         print('trans_hfq ...')
         def hfq(group):
             ''' 后复权 '''
-            # 如果股票没有最近一天的数据，则过滤
-            # filtered_group = group[group['date'] == self.end_date]
-            # if filtered_group.empty:
-            #     return None
-            # 对open, close, high, low复权
-            group['open'] = group['open'] * group['factor']
-            group['close'] = group['close'] * group['factor']
-            group['high'] = group['high'] * group['factor']
-            group['low'] = group['low'] * group['factor']
+            # 对open, close, high, low, vol 复权
+            group['open'] = group['open'] * group['adj_factor']
+            group['close'] = group['close'] * group['adj_factor']
+            group['high'] = group['high'] * group['adj_factor']
+            group['low'] = group['low'] * group['adj_factor']
+            group['vol'] = (group['vol'] / group['adj_factor'])
             return group
 
         def qfq(group):
@@ -102,19 +107,20 @@ class Processor:
             filtered_group = group[group['date'] == self.end_date]
             if filtered_group.empty:
                 return None
-            now_factor = filtered_group['factor'].iloc[0]
+            now_factor = filtered_group['adj_factor'].iloc[0]
             # 对open, close, high, low复权
-            group['open'] = group['open'] * group['factor'] / now_factor
-            group['close'] = group['close'] * group['factor'] / now_factor
-            group['high'] = group['high'] * group['factor'] / now_factor
-            group['low'] = group['low'] * group['factor'] / now_factor
-            group['factor'] = group['factor'] / now_factor
+            group['open'] = group['open'] * group['adj_factor'] / now_factor
+            group['close'] = group['close'] * group['adj_factor'] / now_factor
+            group['high'] = group['high'] * group['adj_factor'] / now_factor
+            group['low'] = group['low'] * group['adj_factor'] / now_factor
+            group['adj_factor'] = group['adj_factor'] / now_factor
+            group['vol'] = group['vol'] * now_factor / group['adj_factor']
             return group
 
         if self.fq == 'hfq':
-            df = df.groupby(by='ts_code').apply(hfq)
+            df = df.groupby(by='ts_code', group_keys=False).apply(hfq)
         elif self.fq == 'qfq':
-            df = df.groupby(by='ts_code').apply(qfq)
+            df = df.groupby(by='ts_code', group_keys=False).apply(qfq)
         return df
 
     def split_stock_data(self, df):
@@ -128,33 +134,44 @@ class Processor:
 
         # 按股票代码分组并保存
         for ts_code, group in df.groupby('ts_code'):
-            ts_code = '{}{}'.format(ts_code.split('.')[1], ts_code.split('.')[0])
-            output_file = os.path.join(output_dir, f"{ts_code}.csv")
+            code = '{}{}'.format(ts_code.split('.')[1], ts_code.split('.')[0])
+            output_file = os.path.join(output_dir, f"{code}.csv")
             group.to_csv(output_file, index=False, columns=self.columns[1:])
             print(f"生成: {output_file} ({len(group)}行)")
 
     def main(self):
-        dt = time.time()
-        if not is_trade_day(self.end_date):
-            print('end_date must be trade day: {}'.format(self.end_date))
-            return
+        try:
+            if not is_trade_day(self.end_date):
+                print('end_date must be trade day: {}'.format(self.end_date))
+                return
 
-        df = self.load_data()
-        index_list = ['000300.SH', '000905.SH', '000903.SH']
-        index_df = self.load_data_index(index_list)
-        if df.empty or index_df.empty:
-            raise ValueError("DataFrames df and index_df cannot be empty")
-        merged = pd.concat([df, index_df], axis=0, ignore_index=True)
-        merged.columns = self.columns
-        merged_fq = self.trans_fq(merged)
-        merged_fq = merged_fq.round({'open': 2, 'close': 2, 'high': 2, 'low': 2, 'factor': 4})
-        # 保存中间数据
-        # merged_fq.to_csv(os.path.join(self.provider_uri, 'hfq_{}_{}.csv'.format(self.start_date, self.end_date)), index=False)
+            df = self.load_data()
+            index_df = self.load_data_index(self.index_list)
+            if df.empty or index_df.empty:
+                raise ValueError("DataFrames df and index_df cannot be empty")
+            merged = pd.concat([df, index_df], axis=0, ignore_index=True)
+            merged_fq = self.trans_fq(merged)
+            # merged_fq.reset_index(drop=True, inplace=True)
 
-        merged_fq.reset_index(drop=True, inplace=True)
-        print(merged_fq.head())
-        self.split_stock_data(merged_fq)
-        print('耗时：{} s'.format(round(time.time() - dt, 4)))
+            # 单位转化
+            merged_fq['vol'] = merged_fq['vol'] * 100  # 单位股
+            merged_fq['amount'] = merged_fq['amount'] * 1000  # 单位元
+
+            # change字段
+            def get_change(group):
+                group.sort_values(by='date', inplace=True)
+                group['change'] = (group['close'] - group['close'].shift(1)) / group['close'].shift(1)
+                return group
+            merged_fq = merged_fq.groupby(by='ts_code', group_keys=False).apply(get_change)
+            merged_fq.columns = self.columns
+            merged_fq = merged_fq.round({'open': 2, 'close': 2, 'high': 2, 'low': 2, 'volume': 2, 'amount': 3, 'change': 4, 'factor': 4})
+            # 保存中间数据
+            # merged_fq.to_csv(os.path.join(self.provider_uri, 'hfq_{}_{}.csv'.format(self.start_date, self.end_date)), index=False)
+            print(merged_fq.head())
+            self.split_stock_data(merged_fq)
+        except Exception as e:
+            error_info = traceback.format_exc()
+            send_email('Data:qlib_online:process', error_info)
 
 if __name__ == '__main__':
     '''
@@ -168,4 +185,6 @@ if __name__ == '__main__':
         每日更新：
         python process.py main --start_date 2025-08-01 --end_date 2025-08-01
     '''
+    dt = time.time()
     fire.Fire(Processor)
+    print('耗时：{} s'.format(round(time.time() - dt, 4)))
