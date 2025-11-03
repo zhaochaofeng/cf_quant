c059e05bc13dcc92c9c44651bd2df0a3444cec33
zhaochaofeng <469293319@qq.com>
2025-11-03 17:53:04 +0800

    stock_info_ts add industry info

---
+++ data/stock_info/run_ts2.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+source ~/.bashrc
+cur_path=`pwd`
+python_path="/root/anaconda3/envs/python3/bin/python"
+cf_quant_path="/root/cf_quant"
+echo 'cur_path: '${cur_path}
+
+dt=`date +%Y-%m-%d`
+echo "dt: "${dt}
+
+${python_path} ${cf_quant_path}/utils/is_trade_date.py ${dt}
+if [ $? -eq 5 ];then
+  echo '非交易日，退出！'
+  exit 0
+fi
+
+${python_path} ${cur_path}/stock_info_ts2.py ${dt}
+
+# 判断是否执行成功
+if [ $? -eq 0 ]; then
+  echo "执行成功！！！"
+  exit 0
+ else
+  echo "执行失败！！！"
+  exit 1
+fi
---
+++ data/stock_info/stock_info_ts2.py
@@ -0,0 +1,153 @@
+'''
+    股票基本上信息表：stock_info_ts
+'''
+import time
+import traceback
+from datetime import datetime
+
+import fire
+import pandas as pd
+from data.process_data import Base, ts_api
+from utils import tushare_pro, send_email
+
+feas = {
+    'ts_code': 'ts_code',
+    'qlib_code': 'ts_code',
+    'day': '',
+    'name': 'name',
+    'exchange': 'ts_code',
+    'status': 'status',
+    'area': 'area',
+    'industry': 'industry',
+    'cnspell': 'cnspell',
+    'market': 'market',
+    'list_date': 'list_date',
+    'act_name': 'act_name',
+    'act_ent_type': 'act_ent_type',
+    'l1_code': 'l1_code',
+    'l1_name': 'l1_name',
+    'l2_code': 'l2_code',
+    'l2_name': 'l2_name',
+    'l3_code': 'l3_code',
+    'l3_name': 'l3_name',
+    'in_date': 'in_date',
+    'out_date': 'out_date',
+    'is_new': 'is_new'
+}
+exchange_map = {'BJ': 'BSE', 'SH': 'SSE', 'SZ': 'SZSE'}
+exclude_codes = {'T00018.SH'}  # 噪声数据
+
+
+class TSStockInfoProcessor(Base):
+    ''' 股票信息数据 '''
+
+    def __init__(self,
+                 feas: dict,
+                 table_name: str,
+                 date: str = None,
+                 **kwargs
+                 ):
+        super().__init__(feas=feas, table_name=table_name, **kwargs)
+        self.date = date
+
+    def fetch_data_from_api(self):
+        ''' 股票基本信息 + 行业数据 '''
+        self.logger.info('\n{}\n{}'.format('=' * 100, 'fetch_data_from_api ...'))
+        try:
+            pro = tushare_pro()
+            # 基本信息
+            df_info = pd.DataFrame()
+            status = {'L': 1, 'D': 0, 'P': 2}
+            for k, v in status.items():
+                tmp = ts_api(pro, 'stock_basic', list_status=k)
+                tmp['status'] = v
+                df_info = pd.concat([df_info, tmp], ignore_index=True)
+            df_info.set_index('ts_code', inplace=True)
+            # 排除噪声数据
+            df_info = df_info[~df_info.index.isin(exclude_codes)]
+            self.logger.info('df_info shape: {}'.format(df_info.shape))
+            if df_info.empty:
+                err_msg = 'df_info is empty !'
+                self.logger.error(err_msg)
+                raise Exception(err_msg)
+
+            # 行业数据
+            df_ind = pd.DataFrame()
+            # 一级行业代码集合
+            ind_code = pro.index_classify(level='L1', src='SW2021')['index_code'].values.tolist()
+            for code in ind_code:
+                tmp = ts_api(pro, 'index_member_all', l1_code=code)
+                df_ind = pd.concat([df_ind, tmp], ignore_index=True)
+                df_ind.drop(columns='name', inplace=True)
+            df_ind.set_index('ts_code', inplace=True)
+            self.logger.info('df_ind shape: {}'.format(df_ind.shape))
+            if df_ind.empty:
+                err_msg = 'df_ind is empty !'
+                self.logger.error(err_msg)
+                raise Exception(err_msg)
+
+            df_ind = df_ind.reindex(df_info.index)
+            df_merge = pd.concat([df_info, df_ind], axis=1, join='outer')
+            df_merge.reset_index(inplace=True)
+            self.logger.info('df_merge shape: {}'.format(df_merge.shape))
+
+            return df_merge
+        except Exception as e:
+            error_msg = 'error in fetch_data_from_api: {}'.format(e)
+            self.logger.error(error_msg)
+            raise Exception(error_msg)
+
+    def parse_line(self, row) -> dict:
+        ''' 解析单条数据 '''
+        try:
+            tmp = {}
+            for f in self.feas.keys():
+                if f == 'day' and self.feas[f] == '':
+                    if self.date:
+                        v = self.date
+                    elif self.date is None:
+                        # 对于没有日期的表，用取数当天日期
+                        v = datetime.now().strftime('%Y-%m-%d')
+                else:
+                    v = row[self.feas[f]]
+                    if pd.isna(v):
+                        v = None
+                    elif f == 'qlib_code':
+                        code, suffix = v.split('.')
+                        v = '{}{}'.format(suffix.upper(), code)
+                    elif f in ['day', 'in_date']:
+                        # 日期格式转换
+                        v = datetime.strptime(v, '%Y%m%d').strftime('%Y-%m-%d')
+                    elif f == 'exchange':
+                        code, suffix = v.split('.')
+                        v = exchange_map[suffix]
+                tmp[f] = v
+            return tmp
+        except Exception as e:
+            error_msg = 'parse_line error: {}'.format(e)
+            self.logger.error(error_msg)
+            raise Exception(error_msg)
+
+
+def main(date: str = None) -> None:
+    t = time.time()
+    processor = TSStockInfoProcessor(
+        feas=feas,
+        table_name='stock_info_ts2',
+        log_file='log/{}.log'.format(datetime.now().strftime('%Y-%m-%d')),
+        date=date,
+    )
+    try:
+        df = processor.fetch_data_from_api()
+        data = processor.process(df)
+        processor.write_to_mysql(data)
+        print('耗时：{}s'.format(round(time.time() - t, 4)))
+    except:
+        error_msg = traceback.format_exc()
+        processor.logger.error(error_msg)
+        send_email('Data: stock_info_ts', error_msg)
+        raise Exception(error_msg)
+
+
+if __name__ == '__main__':
+    fire.Fire(main)
