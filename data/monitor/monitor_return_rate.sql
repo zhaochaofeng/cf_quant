cddff34fee431cff85aca5a663ecf7198090ec9c
zhaochaofeng <469293319@qq.com>
2025-12-02 10:25:05 +0800

    add fields model, instruments

--- data/monitor/monitor_return_rate.sql
+++ data/monitor/monitor_return_rate.sql
@@ -4,6 +4,8 @@ CREATE TABLE IF NOT EXISTS monitor_return_rate(
     qlib_code VARCHAR(10) NOT NULL COMMENT 'Qlib股票代码, 如 SZ000001',
     day DATE NOT NULL COMMENT '取数日期，格式YYYY-MM-DD',
     score Decimal(10, 6) NOT NULL COMMENT '预测得分',
+    model VARCHAR(50) COMMENT '模型名称, 如 lightgbm_alpha',
+    instruments VARCHAR(50) COMMENT '股票集合，如 csi300',
     PRIMARY KEY (id),
     UNIQUE KEY un_code_day (qlib_code, day)
 ) ENGINE=InnoDB
@@ -11,6 +13,4 @@ CREATE TABLE IF NOT EXISTS monitor_return_rate(
     COLLATE=utf8mb4_unicode_ci
     COMMENT='预测收益率'
     AUTO_INCREMENT=1;
-
-
 
