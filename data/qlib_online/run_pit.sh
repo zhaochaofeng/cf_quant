e1fdbd411772ae57e71c17efc43684ad70aff0b7
zhaochaofeng <469293319@qq.com>
2025-12-12 20:30:04 +0800

    Change dt1

--- data/qlib_online/run_pit.sh
+++ data/qlib_online/run_pit.sh
@@ -12,7 +12,7 @@ provider_uri="${data_path}/custom_data_hfq"
 echo "cur_path: ${cur_path}"
 
 if [ $# -eq 0 ]; then
-    dt1='2015-01-05'
+    dt1='2008-01-02'
     dt2=`date +%Y-%m-%d`
   elif [ $# -eq 1 ]; then
     dt1=$1
