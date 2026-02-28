7463d5d3e10a58230a417e8ec20c1ec85493de1c
zhaochaofeng <469293319@qq.com>
2026-02-28 15:03:01 +0800

    Add winsorize and standarize to MIDCAP

--- data/factor/size.py
+++ data/factor/size.py
@@ -4,6 +4,7 @@
 
 import numpy as np
 from utils import WLS
+from utils import winsorize, standardize
 
 
 def LNCAP(df):
@@ -27,6 +28,8 @@ def MIDCAP(df):
     beta, alpha, resid = WLS(y, x, intercept=True, weight=1, verbose=True)
     # 这里使用的 lncap 以保证原始数据维度
     midcap = lncap ** 3 - (alpha + beta[0] * lncap)
+    midcap = winsorize(midcap, method='quantile', lower=0.01, upper=0.99)
+    midcap = standardize(midcap)
     midcap = midcap.to_frame()
     midcap.columns = ['MIDCAP']
 
