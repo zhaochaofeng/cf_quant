f42237636ff73df8d2394dcfc9a1988532ec1baa
zhaochaofeng <469293319@qq.com>
2026-05-13 16:20:46 +0800

    test deploy

--- data/stock_info/test.py
+++ data/stock_info/test.py
@@ -1,17 +1,20 @@
+import time
 from prefect import flow
+from prefect.schedules import Schedule
 
 @flow(log_prints=True)
-def my_flow(name: str = 'flow'):
-    print(f'hello {name}')
+def my_flow():
+    print(f'hello flow {time.time()}')
 
 if __name__ == '__main__':
-    my_flow.from_source(   # 指定 flow 函数代码路径
-    # source=".",
-    # entrypoint="test.py:my_flow"
+    schedule = Schedule(
+        cron="*/1 * * * *"
+    )
+    my_flow.from_source(
     source="https://github.com/zhaochaofeng/cf_quant.git",
-    entrypoint="flows.py:my_flow",
+    entrypoint="data/stock_info/test.py:my_flow",
     ).deploy(
-        name='my-flow',
+        name='my-flow-test',
         work_pool_name='cf_quant',   #  指定 work pool
     )
 
