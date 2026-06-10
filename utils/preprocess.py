04b98dd38fc45d24957f258329a04089caf77b4f
zhaochaofeng <469293319@qq.com>
2026-06-10 10:23:58 +0800

    Delete func exec flag log

--- utils/preprocess.py
+++ utils/preprocess.py
@@ -137,7 +137,6 @@ def winsorize(
     Returns:
         pd.DataFrame: 去极值后的数据
     """
-    logger.info('winsorize ...')
     df = _to_dataframe(data)
 
     # 无分组：全局去极值
@@ -197,7 +196,6 @@ def standardize(
     Returns:
         pd.DataFrame: 标准化后的数据
     """
-    logger.info('standardize ...')
     df = _to_dataframe(data)
 
     # 无分组：全局标准化
@@ -322,7 +320,6 @@ def neutralize(
     Returns:
         中性化后的残差，维度和类型与y一致
     """
-    logger.info('neutralize ...')
     is_series = isinstance(y, pd.Series)
     is_1d_array = isinstance(y, np.ndarray) and y.ndim == 1
     is_nd_array = isinstance(y, np.ndarray) and y.ndim > 1
