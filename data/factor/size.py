9df9da7108f430e08b96fcf6374d56f591309bfa
zhaochaofeng <469293319@qq.com>
2026-06-25 19:28:22 +0800

    Fix: neutralize y by level datetime

--- data/factor/size.py
+++ data/factor/size.py
@@ -3,40 +3,38 @@
 '''
 
 import numpy as np
+import pandas as pd
 from utils import WLS
-from utils import winsorize, standardize
-from utils.dt import time_decorator
+from utils import winsorize, standardize, neutralize
 
 
-@time_decorator
-def LNCAP(df):
-    """ 市值 """
+def LNCAP(df) -> pd.Series:
+    """ 市值(大小盘) """
 
-    df = df.sort_index()
     circ_mv = df['$circ_mv']
     lncap = np.log(circ_mv)
     lncap.name = 'LNCAP'
-    result_df = lncap.to_frame()
+    lncap.dropna(inplace=True)
+    lncap.sort_index(inplace=True)
 
-    return result_df
+    return lncap
 
 
-@time_decorator
 def MIDCAP(df):
     """ 中等市值 """
 
-    lncap = LNCAP(df)['LNCAP']
-    x = lncap.dropna().values
-    y = x ** 3
-    beta, alpha, resid = WLS(y, x, intercept=True, weight=1, verbose=True)
-    # 这里使用的 lncap 以保证原始数据维度
-    midcap = lncap ** 3 - (alpha + beta[0] * lncap)
-    midcap = winsorize(midcap, method='median', lower=0.01, upper=0.99)
-    midcap = standardize(midcap)
+    circ_mv = df['$circ_mv']
+    lncap = np.log(circ_mv).dropna()
+
+    y = lncap ** 3
+    midcap = neutralize(y, lncap, intercept=True, level='datetime')
+    midcap = midcap.to_frame()
+    midcap = winsorize(midcap, method='median', lower=0.01, upper=0.99, level='datetime')
+    midcap = standardize(midcap, level='datetime')
     midcap.columns = ['MIDCAP']
+    midcap.sort_index(inplace=True)
 
     return midcap
-
 
 
 
