37ad6c214c034ea7b8ed077297149f4421a74d42
zhaochaofeng <469293319@qq.com>
2026-06-26 14:07:23 +0800

    Change field from total_mv to circ_mv

--- data/factor/size.py
+++ data/factor/size.py
@@ -13,8 +13,8 @@ from utils.dt import time_decorator
 @factor_output
 def LNCAP(df) -> pd.Series:
     """ 市值(大小盘) """
-    total_mv = df['$total_mv']
-    lncap = np.log(total_mv)
+    field = df['$circ_mv']
+    lncap = np.log(field)
     return lncap
 
 
@@ -22,7 +22,6 @@ def LNCAP(df) -> pd.Series:
 @factor_output
 def MIDCAP(df) -> pd.Series:
     """ 中等市值
-
     import qlib
     from qlib.data import D
     from utils import neutralize
@@ -36,9 +35,9 @@ def MIDCAP(df) -> pd.Series:
     instruments = D.list_instruments(
         instruments, start_time=start_time, end_time=end_time, as_list=True
     )
-    total_mv = D.features(instruments, ['$total_mv'], start_time=start_time, end_time=end_time)
-    total_mv.columns = ['total_mv']
-    lncap = np.log(total_mv * 10000).dropna()
+    circ_mv = D.features(instruments, ['$circ_mv'], start_time=start_time, end_time=end_time)
+    circ_mv.columns = ['circ_mv']
+    lncap = np.log(circ_mv * 10000).dropna()
     y = lncap ** 3
 
     resid = neutralize(y, lncap, intercept=True, level='datetime')
@@ -51,18 +50,18 @@ def MIDCAP(df) -> pd.Series:
     # 可以观察到。中盘股残差均值小于0，下盘股和大盘股残差均值都大于0
     lncap           小盘股           中小盘           中盘股           大中盘           大盘股
     count  23735.000000  23734.000000  23734.000000  23734.000000  23734.000000
-    mean     130.578505     15.974275    -53.389554   -103.583552     10.414824
-    std       68.157654     22.307908     18.372853      9.826758    241.394774
-    min       48.833205    -26.709102    -86.267268   -115.987386   -115.613872
-    25%       84.484272     -2.996741    -69.700754   -112.654295   -106.551545
-    50%      116.784224     15.015561    -54.300882   -105.716003    -78.629203
-    75%      159.795758     34.802384    -37.525170    -96.004140     12.282583
-    max     1194.794017     62.499206    -16.560729    -80.045903   1864.057956
+    mean     137.879820     -2.270755    -66.843147   -103.865496     35.093769
+    std       91.857469     22.525693     15.585970      5.199230    236.510209
+    min       32.801393    -42.817697    -94.268194   -110.015085   -105.848627
+    25%       67.822948    -22.138199    -80.098711   -108.027798    -89.501895
+    50%      110.278836     -3.437104    -67.897848   -106.017824    -51.170542
+    75%      181.711688     16.928683    -53.762151   -100.298271     56.080001
+    max     1009.902285     44.348441    -34.661370    -89.483353   2003.726138
 
     """
 
-    total_mv = df['$total_mv']
-    lncap = np.log(total_mv).dropna()
+    field = df['$circ_mv']
+    lncap = np.log(field).dropna()
 
     y = lncap ** 3
     midcap = neutralize(y, lncap, intercept=True, level='datetime')
