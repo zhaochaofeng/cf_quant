1627fdd028cd485d3550a54c736dcfbaee970955
zhaochaofeng <469293319@qq.com>
2026-07-23 10:57:18 +0800

    Set default value of n-hof to 1000

--- data/factor_gp/pipeline.py
+++ data/factor_gp/pipeline.py
@@ -304,7 +304,7 @@ def main():
     parser.add_argument('--n-pop', type=int, default=10, help='每岛种群大小')
     parser.add_argument('--n-gen', type=int, default=4, help='进化代数')
     parser.add_argument('--n-islands', type=int, default=3, help='岛屿数量')
-    parser.add_argument('--n-hof', type=int, default=10, help='每个岛屿筛选表达式数量')
+    parser.add_argument('--n-hof', type=int, default=1000, help='每个岛屿筛选表达式数量')
     parser.add_argument('--no-llm', action='store_true', help='禁用 LLM')
     parser.add_argument('--kernels', type=int, default=1, help='qlib kernels')
     parser.add_argument('--seed', type=int, default=42)
